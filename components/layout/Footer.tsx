7ddccd2c55e383ac5d884cc55ae199a663a39ca4
brahito <brayan@ilogica.cl>
2023-10-07 15:22:36 -0500

    add page proyectos finished

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,7 +1,39 @@
+import Link from 'next/link'
 import React from 'react'
 
 export const Footer = () => {
   return (
-    <div></div>
+    <footer>
+      <div className='container'>
+
+        <div className='content'>
+          <h2>SERGIO PEÑALOZA</h2>
+          <span className='line-v'></span>
+          <div className='d-flex align-items-center nav'>
+            <Link href="/proyectos">
+              Work
+            </Link>
+            <Link href="/proyectos">
+              About
+            </Link>
+            <Link href="/proyectos">
+              Contact
+            </Link>
+          </div>
+        </div>
+        <span className='line-h'></span>
+        <div className='post'>
+          <p>Copyright © 2023 Sergio Peñaloza</p>
+          <span className='line-v'></span>
+          <div className='redes'>
+            <p>Follow me:</p>
+            <a href="https://www.instagram.com/checholiolin/" target='_blank'><i className="ri-instagram-fill"></i></a>
+            <a href="https://www.artstation.com/xexolio" target='_blank'><img src="./images/artstation.svg" alt="" /></a>
+            <a href="https://www.linkedin.com/in/sergio-pe%C3%B1aloza-herrera-137436183/" target='_blank'><i className="ri-linkedin-box-fill"></i></a>
+          </div>
+        </div>
+      </div>
+
+    </footer>
   )
 }
--- components/sections/Know.tsx
+++ components/sections/Know.tsx
@@ -10,6 +10,23 @@ export const Know = () => {
           <div className='img'>
             <Image src={"/images/esteviejocachondohijodeputacogecomountorosinusarpastillas.jpg"} width={200} objectFit={"cover"} height={200} alt='Sergio Peñaloza' />
           </div>
+          <div className='cards'>
+            <div className='card_know'>
+              <span className='icon'><i className="ri-user-line"></i></span>
+              <h3>WHO AM I?</h3>
+              <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna.</p>
+            </div>
+            <div className='card_know'>
+              <span className='icon'><i className="ri-flag-line"></i></span>
+              <h3>WHAT DO I DO?</h3>
+              <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna.</p>
+            </div>
+            <div className='card_know'>
+              <span className='icon'><i className="ri-award-line"></i></span>
+              <h3>HOW DO I DO IT?</h3>
+              <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna.</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
